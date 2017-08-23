3544ec958b0816a39a1e1adc63d0af6f305a81dd
Ruchira (Richie) Jayasekara <richie.jayasekara@myob.com>
2017-08-23 19:17:36 +1000

    Added new service

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 GIT_REPOS=(
 	"https://github.com/ruchira088/url-alias-api.git" 
 	"https://github.com/ruchira088/url-alias-web.git"
+	"https://github.com/ruchira088/url-alias-user.git"
 )
 
 for gitRepo in "${GIT_REPOS[@]}"
